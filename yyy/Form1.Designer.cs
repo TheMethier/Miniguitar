d4419d7667c19d4c79f161303929d6b15cbbae4f
TheMethier <maciej,kujawa202@gmail.com>
2022-06-02 20:44:06 +0100

    Dodaj pliki projektów.

---
+++ yyy/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace yyy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Hitbox = new System.Windows.Forms.PictureBox();
+            this.note1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hitbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.note1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 188);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1069, 135);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Hitbox
+            // 
+            this.Hitbox.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.Hitbox.Location = new System.Drawing.Point(63, 188);
+            this.Hitbox.Name = "Hitbox";
+            this.Hitbox.Size = new System.Drawing.Size(136, 134);
+            this.Hitbox.TabIndex = 1;
+            this.Hitbox.TabStop = false;
+            this.Hitbox.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // note1
+            // 
+            this.note1.Image = global::yyy.Properties.Resources.note;
+            this.note1.Location = new System.Drawing.Point(921, 202);
+            this.note1.Name = "note1";
+            this.note1.Size = new System.Drawing.Size(100, 100);
+            this.note1.TabIndex = 2;
+            this.note1.TabStop = false;
+            this.note1.Tag = "note1";
+            this.note1.Click += new System.EventHandler(this.note_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(53, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Score:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.GameEvent);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::yyy.Properties.Resources.note;
+            this.pictureBox2.Location = new System.Drawing.Point(315, 202);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "note";
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.note1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Hitbox);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyisDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyp);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyUP);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hitbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.note1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox Hitbox;
+        private System.Windows.Forms.PictureBox note1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ yyy/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace yyy
+{public partial class Form1 : Form
+    {
+        int Score = 0;
+        int ospeed = 0;
+        int sec = 0;
+        bool press=false;
+        bool win = false;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            Run();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {}
+        private void GameEvent(object sender, EventArgs e)
+        {
+            if (press) sec++;
+            label1.Text = "Score: " + Score;
+            foreach(Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "note")
+                { 
+                    x.Left -= ospeed;
+                    if (x.Left < pictureBox1.Left)
+                    {
+                        Run();
+                    }
+                    if (sec<3&&press&&x.Left<Hitbox.Right)
+                    {
+                        win = true; }
+                }
+              }  
+        }
+        private void KeyUP(object sender, KeyEventArgs e)
+        {
+            if(press)
+            {
+                press = false;
+                Hitbox.BackColor = Color.BlanchedAlmond;
+            }
+        }
+        private void KeyisDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Space && !press)
+            {
+                press = true;
+                Hitbox.BackColor=Color.Green;
+}
+        }
+        public void Run()
+        {if (win) { Score++; }
+            
+            win = false;
+            ospeed = 10+Score/(3);
+            sec = 0;
+            Hitbox.Top = pictureBox1.Top;
+            foreach(Control x in Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "note")
+               {
+                    int pos = random.Next(63, 700);
+                    x.Left = pos + Hitbox.Width;
+                }
+            }
+            timer1.Start();
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+        private void keyp(object sender, KeyPressEventArgs e)
+        {
+        }
+        private void note_Click(object sender, EventArgs e)
+        {
+        }
+        private void pictureBox2_Click_1(object sender, EventArgs e)
+        {
+        }
+        private void pictureBox2_Click_2(object sender, EventArgs e)
+        {
+        }
+    }
+}
